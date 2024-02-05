d74d7a215a37f9808a9f2555a394872fbaa8e95f
Darshil Dobariya <89260736+darshil44@users.noreply.github.com>
2024-02-05 15:43:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const setofWords = 
+[
+    "If you're visiting this page, you're likely here because you're searching for a random sentence",
+    "Sometimes a random word just isn't enough, and that is where the random sentence generator comes into play.",
+    "By inputting the desired number, you can make a list of as many random sentences as you want or need.",
+    "Producing random sentences can be helpful in a number of different ways.",
+    "For writers, a random sentence can help them get their creative juices flowing",
+    "Since the topic of the sentence is completely unknown, it forces the writer to be creative when the sentence appears. There are a number of different ways a writer can use the random sentence for creativity."
+];
+const msg = document.getElementById('msg')
+const typeWords = document.getElementById('mywords')
+const button= document.getElementById('btn')

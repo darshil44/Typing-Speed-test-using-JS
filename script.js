e9411830785ef03da36058c00ac2b5d0f30a3df4
Darshil Dobariya <89260736+darshil44@users.noreply.github.com>
2024-02-06 11:54:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,47 @@
-const setofWords = 
-[
-    "If you're visiting this page, you're likely here because you're searching for a random sentence",
-    "Sometimes a random word just isn't enough, and that is where the random sentence generator comes into play.",
-    "By inputting the desired number, you can make a list of as many random sentences as you want or need.",
-    "Producing random sentences can be helpful in a number of different ways.",
-    "For writers, a random sentence can help them get their creative juices flowing",
-    "Since the topic of the sentence is completely unknown, it forces the writer to be creative when the sentence appears. There are a number of different ways a writer can use the random sentence for creativity."
-];
-const msg = document.getElementById('msg')
-const typeWords = document.getElementById('mywords')
-const button= document.getElementById('btn')
+document.addEventListener('DOMContentLoaded', function () {
+
+    const setofWords = [
+        "If you're visiting this page, you're likely here because you're searching for a random sentence",
+        "Sometimes a random word just isn't enough, and that is where the random sentence generator comes into play.",
+        "By inputting the desired number, you can make a list of as many random sentences as you want or need.",
+        "Producing random sentences can be helpful in a number of different ways.",
+        "For writers, a random sentence can help them get their creative juices flowing",
+        "Lulu was out for her usual morning walk when she took a wrong turn and found herself in the woods.",
+        "She tried to retrace her steps, but soon realized she was lost.",
+        "She began to panic, but then she remembered her training and started using her nose to sniff out a way home."
+    ];
+
+    const msg = document.getElementById('msg');
+    const typeWords = document.getElementById('mywords');
+    const button = document.getElementById('btn');
+    let startTime, endTime;
+
+    const playGame = () => {
+        let randomNumber = Math.floor(Math.random() * setofWords.length);
+        console.log(randomNumber);
+        msg.innerText = setofWords[randomNumber];
+        let date = new Date();
+        startTime = date.getTime();
+        btn.innerText = 'Done';
+    }
+    const endPlay = () => {
+        let date = new Date();
+        endTime = date.getTime();
+        let totalTime = ((endTime - startTime) / 1000 );
+        console.log(totalTime);
+    }
+
+button.addEventListener('click', function () {
+    console.log(this.innerText);
+    if (this.innerText === 'Start') {
+        typeWords.disabled = false;
+        playGame();
+    }
+    else if (this.innerText == 'Done') {
+        typeWords.disabled = true;
+        button.  = 'Start'
+        endPlay();
+    }
+
+});
+});
